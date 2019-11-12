f5423c71d0826738836a24e129e55fe2c300b0c1
Konstantin Schuckmann <kn.schuckmann@gmx.de>
2019-11-12 22:47:37 +0100

    Not quite sure because the output seems still not to be right

--- lfi-02/image_retrieval.py
+++ lfi-02/image_retrieval.py
@@ -2,13 +2,18 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Nov  7 17:46:10 2019
+This function calculates SIFT descriptors for images using uniform distributed keypoints.
+It also ques the results so one can have a better glance on similar pictures using the given Database.
 
+Information and Sources:
+    * The glob module finds all the pathnames matching a specified 
+        pattern according to the rules used by the Unix shell, although 
+        results are returned in arbitrary order
+    
+    
 @author: Konstantin Schuckmann
 """
 import cv2
-# The glob module finds all the pathnames matching a specified 
-# pattern according to the rules used by the Unix shell, although 
-# results are returned in arbitrary order
 import glob
 import numpy as np
 import os
@@ -21,17 +26,18 @@ import matplotlib.pyplot as plt
 #
 ############################################################
 
-
-# implement distance function
 def distance(a, b):
-    """Input two pixels 
+    """Input two pixels, or two images 
     calculate the euclidean distance
     Return: Euclidean distance
     """
     return np.linalg.norm(a-b)
 
-
+# not sure if one should mayeb convert the image to gray like in HOG
 def load_image(path):
+    """Input: path to image
+    return: image
+    """
     img = cv2.imread(path, cv2.COLOR_BGR2GRAY)
     return img
 
@@ -46,12 +52,15 @@ def create_keypoints_Uniform(w, h, keypointSize):
     x_dir = np.arange(0,w,keypointSize)
     y_dir = np.arange(0,h,keypointSize)
     keypoints = np.meshgrid(x_dir,y_dir)
-
+    
+    # go through each x, and y point and connect them 
     keypoints = [[keypoints[0][i][j],keypoints[1][i][j]] for i in range(len(x_dir)) for j in range(len(x_dir))]
+    # Reshape the result for further use in cv2.Keypoints
     keypoints = np.reshape(keypoints,(len(keypoints),2))
+    
     return keypoints
 
-
+# only used on my machine for the direction
 os.chdir("/Users/Kostja/Desktop/Master/Sem 5 (19:20 WiSe)/Learning from Images/Assignments/lfi-02")
 
 # 1. preprocessing and load
@@ -65,15 +74,14 @@ keypoints = create_keypoints_Uniform(256, 256,keyPointSize)
 # create the keys
 keys = [cv2.KeyPoint(keypoints[i][0], keypoints[i][1], keyPointSize) for i in range(len(keypoints))]
 
-sift = cv2.xfeatures2d.SIFT_create()
-
 # 3. use the keypoints for each image and compute SIFT descriptors
-#    for each keypoint. this compute one descriptor for each image.
-# descriptors is devided in keypoints and descriptors like mentioned in 
+#    for each keypoint. this calculation computes one descriptor for each image.
+#    descriptors are devided in keypoints and descriptors
+sift = cv2.xfeatures2d.SIFT_create()
 descriptors = []
-# 16 x 16 neighbourhood arround keypoints each devided into 16 subblox of the size of 4x4
+# 16 x 16 neighbourhood arround keypoints, each devided into 16 subblocks of the size of 4x4 pixels
 # each of this subblocks has 8 bin orientations
-# result is for each keypoint a 128 ( = 16sobbloxks * 8 bins)  bin values
+# result in 128 ( = 16sobbloxks * 8 bins) bin values for each keypoint 
 descriptors = [sift.compute(images[i], keys) for i in range(len(img))]
 
 # 4. use one of the query input image to query the 'image database' that
@@ -81,42 +89,27 @@ descriptors = [sift.compute(images[i], keys) for i in range(len(img))]
 #    compare the descriptor to each image in the database using the L2-norm
 #    and save the result into a priority queue (q = PriorityQueue())
 q = PriorityQueue()
-#[q.put((distance(descriptors[test][1],descriptors[train][1]), str((test,train)))) for test in range(4) for train in range(4,len(descriptors))]
+
 # 5. output (save and/or display) the query results in the order of smallest distance
+# 4 because there are 4 test images so range does 0, 1, 2, 3
 for test in range(4):
+    # for loop for train images
     for train in range(4,len(descriptors)):
         q.put((distance(descriptors[test][1],descriptors[train][1]),[img[test],img[train]]))
 
-while not q.empty():        
-    print(q.get()[0])
-    
-
-for i in range(4):
-
-img = load_image(q.queue[0][1][0])
-img2 = []
-for j in range(len(q.queue)):
-    img2 = load_image(q.queue[j][1][1])
-    
-
-    
-cv2.imshow('as',)np.concatenate((,),axis = 1)
+# can be used but sure yet if it is right but it displays the que from small to big
+#while not q.empty():        
+#    print(q.get()[0])
+      
+########################################################
+# here follows a possibility to display the images
+# but it seems not to be right
+# possible reasons make first picture apper on left followed by 4 times 2x10 picture like
+# in figure of exercise
 
 nr = np.arange(80)
 
-fig, axs = plt.subplots(nrows=4, ncols=20, figsize=(9, 6),
-                        subplot_kw={'xticks': [], 'yticks': []})
-
+fig, axs = plt.subplots(nrows=4, ncols=20, figsize=(9, 6),subplot_kw={'xticks': [], 'yticks': []})
 
 for ax, nr1 in zip(axs.flat, nr):
     ax.imshow(load_image(q.queue[nr1][1][1]))
-
-plt.tight_layout()
-plt.show()
-
-i1 = load_image(q.get()[1][0])
-i2 = load_image(q.get()[1][1])
-i = np.concatenate((i1,i2), axis = 1)
-cv2.imshow('seds',i)
-
-plt.figure()
