e079b345f2be71e546be69bc5163c7546726cb42
Konstantin Schuckmann <kn.schuckmann@gmx.de>
2019-11-25 09:34:43 +0100

    Created the plots so there is a possibility to plot the images

--- lfi-03/svm.py
+++ lfi-03/svm.py
@@ -1,8 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 24 18:08:29 2019
+
+@author: Kostja
+"""
+
 import numpy as np
 import cv2
 import glob
 from sklearn import svm
+import matplotlib.pyplot as plt
 
+import os
+from itertools import groupby 
 
 ############################################################
 #
@@ -11,9 +22,54 @@ from sklearn import svm
 #
 ############################################################
 
+def load_image(path):
+    """Input: path to image
+    return: image
+    """
+    img = cv2.imread(path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    return img
+
+def find_index_Img(paths, startDiffer):
+    """Input: paths of inages and the starting point from where the strings schould differ
+    Return a list with points where successive images differ
+    """
+    group = []
+    length = len(paths)
+    for i in range(length - 1):
+        if imgPaths[i][startDiffer:startDiffer + 2] != imgPaths[i + 1][startDiffer:startDiffer + 2]:
+            group += [i+1]
+    return group
+
+def plot_imgs_with_subplots(rows,cols, group, imgs, axs):
+    """Input 
+    Return 
+    """
+    imgNr = 0
+    for i in range(rows):
+        for j in range(cols):
+            if imgNr in group or imgNr == len(imgs):
+                imgNr += 1
+                break
+            axs[i,j].imshow(imgs[imgNr])
+            imgNr += 1        
+    pass
+    
+# Set the absolute path to work on the right folder
+os.chdir("/Users/Kostja/Desktop/Master/Sem 5 (19:20 WiSe)/Learning from Images/Assignments/lfi-03/")
+
 
 # 1. Implement a SIFT feature extraction for a set of training images ./images/db/train/** (see 2.3 image retrieval)
 # use 256x256 keypoints on each image with subwindow of 15x15px
+
+imgPaths = glob.glob("./images/db/train/*/*")
+groups = find_index_Img(imgPaths, 18)
+images = [load_image(imgPaths[i]) for i in range(len(imgPaths))]    
+fig, axs =  plt.subplots(nrows=3, ncols=10,figsize=(13, 4),subplot_kw={'xticks': [], 'yticks': []})
+plot_imgs_with_subplots(3,10,groups, images, axs)
+
+axs[0,9].imshow(images[1])
+
 
 # 2. each descriptor (set of features) need to be flattened in one vector
 # That means you need a X_train matrix containing a shape of (num_train_images, num_keypoints*num_entry_per_keypoint)
