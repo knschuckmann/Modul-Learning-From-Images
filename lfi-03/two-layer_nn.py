5f5d653746cd714f74930ebf57efccdb63e3d092
Konstantin Schuckmann <kn.schuckmann@gmx.de>
2019-12-06 12:27:27 +0100

    Done after a Lot of differnet not really understandable derivatives

--- lfi-03/two-layer_nn.py
+++ lfi-03/two-layer_nn.py
@@ -1,17 +1,37 @@
+"""
+Created on Thu Dez 01 09:01:21 2019
+
+Two Layer NN 
+is a self created two layer NN. With respect to forward and backward propagation 
+the Network gets Trained on batches. 
+
+The derivations with respect to weights and biases are very hard to understand 
+so a video is provided :
+    * youtube.com/watch?v=GlcnxUlrtek
+
+The output is a trained Network and a plot of the loss in Training and Test rounds
+
+@author: Konstantin Schuckmann
+"""
 import numpy as np
 import cv2
 import glob
 import matplotlib.pyplot as plt
 import torch
 
-device = torch.device('cpu')
+# use GPU if available else CPU 
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
 
+# Hyperparameters
 nn_img_size = 32
 num_classes = 3
 learning_rate = 0.0001
 num_epochs = 500
 batch_size = 4
 
+# Which loss function to take
+#loss_mode = 'mse' 
 loss_mode = 'crossentropy' 
 
 loss_train_hist = []
@@ -72,10 +92,10 @@ def setup_train():
     train_images_flowers = glob.glob('./images/db/train/flowers/*.jpg')
     train_images_faces = glob.glob('./images/db/train/faces/*.jpg')
     train_images = [train_images_cars, train_images_flowers, train_images_faces]
-    num_rows = len(train_images_cars)+len(train_images_flowers) +len(train_images_faces)
+    num_rows = len(train_images_cars)+len(train_images_flowers) +len(train_images_faces) 
     X_train = torch.zeros((num_rows, nn_img_size*nn_img_size))
     y_train = torch.zeros((num_rows, num_classes))
-
+    
     counter = 0
     for (label, fnames) in enumerate(train_images):
         for fname in fnames:
@@ -91,71 +111,96 @@ def setup_train():
             X_train[counter] = torch.from_numpy(img.flatten().astype(np.float32))
             
             counter += 1
-
-    # print(y_train)
     return X_train, y_train
 
 def forward(X_batch, y_batch, W1, W2, b1, b2):
     """forward pass in the neural network """
-    ### YOUR CODE ####
-    # please implement the forward pass
-    # 
+    a1 = relu(torch.mm(X_batch, W1) + b1)
+    a2 = torch.mm(a1, W2) + b2
     
-    # the function should return the loss and both intermediate activations
-    #return loss, a2, a1
+    if loss_mode == 'crossentropy':
+        a2 = softmax(a2)
+        loss = loss_crossentropy(a2, y_batch)
+    else:
+        loss = loss_mse(a2,y_batch)
+    # loss and both intermediate activations
+    return loss, a1, a2
 
 def backward(a2, a1, X_batch, y_batch, W2):
     """backward pass in the neural network """
-    # Implement the backward pass by computing
-    # the derivative of the complete function
     # using the chain rule as discussed in the lecture
 
-    # please use the appropriate loss functions 
-    # YOUR CODE HERE
+    # derivatives for W2
+    dm2dw2 = a1
+    
+    if loss_mode == 'crossentropy':
+        dcdm2 = loss_deriv_crossentropy(a2, y_batch)
+    else:
+        dcdm2 = loss_deriv_mse(a2,y_batch)
+
+    
+    # derivatives for W1 only the non calculated terms
+    dm2da1 = W2
+    da1dm1 = relu_derivative(a1)
+    dm1dw1 = X_batch
+    
+    # calculate recurently terms
+    basic = torch.mm(dcdm2,dm2da1.T)
+    final = (basic * da1dm1) 
     
     # function should return 4 derivatives with respect to
     # W1, W2, b1, b2
-    # return dCdW1, dCdW2, dCdb1, dCdb2
+    dCdW1 = torch.mm(dm1dw1.T,final)
+    dCdW2 = torch.mm(dm2dw2.T,dcdm2)
+    dCdb1 = final.sum(axis=0) 
+    dCdb2 = dcdm2.sum(axis=0)
+
+    return dCdW1, dCdW2, dCdb1, dCdb2
 
 def train(X_train, y_train):
     """ train procedure """
     # for simplicity of this execise you don't need to find useful hyperparameter
     # I've done this for you already and every test image should work for the
     # given very small trainings database and the following parameters.
+    # h is the dimenion of hidden Layer
     h = 1500
     std = 0.001
-    # YOUR CODE HERE
+    cols, rows = X_train.shape
     # initialize W1, W2, b1, b2 randomly
     # Note: W1, W2 should be scaled by variable std
-    
+    b1, b2 = torch.randn(1,h), torch.randn(1,num_classes)
+    W1 = torch.randn(rows, h) * std
+    W2 = torch.randn(h, num_classes) * std    
     # run for num_epochs
     for i in range(num_epochs):
-
-        X_batch = None
-        y_batch = None
-
         # use only a batch of batch_size of the training images in each run
         # sample the batch images randomly from the training set
-        # YOUR CODE HERE
+        sample_batch = np.random.choice(cols ,batch_size) # important for no replacement 
+        X_batch = X_train[sample_batch]
+        y_batch = y_train[sample_batch]
         
         # forward pass for two-layer neural network using ReLU as activation function
-        
+        loss, a1 , a2 = forward(X_batch,y_batch, W1,W2,b1,b2)
         # add loss to loss_train_hist for plotting
+        loss_train_hist.append(loss)
         
-        #if i % 10 == 0:
-        #    print("iteration %d: loss %f" % (i, loss))
-
+        if i % 10 == 0:
+            print("iteration %d: loss %f" % (i, loss))
         # backward pass 
+        dCdW1, dCdW2, dCdb1, dCdb2 = backward(a2,a1,X_batch,y_batch,W2)
         
-        # print("dCdb2.shape:", dCdb2.shape, dCdb1.shape)
+        #print("dCdb2.shape:", dCdb2.shape, dCdb1.shape)
 
         # depending on the derivatives of W1, and W2 regaring the cost/loss
         # we need to adapt the values in the negative direction of the 
         # gradient decreasing towards the minimum
         # we weight the gradient by a learning rate
-        # YOUR CODE HERE
+        W1 -= dCdW1 * learning_rate
+        W2 -= dCdW2 * learning_rate
+        b1 -= dCdb1 * learning_rate
+        b2 -= dCdb2 * learning_rate
         
-    # return W1, W2, b1, b2
+    return W1, W2, b1, b2
 
 X_train, y_train = setup_train()
 W1, W2, b1, b2 = train(X_train, y_train)
@@ -170,14 +215,23 @@ test_images.append( (cv2.imread('./images/db/test/face.jpg', cv2.IMREAD_GRAYSCAL
 for ti in test_images:
     resized_ti = cv2.resize(ti[0], (nn_img_size, nn_img_size) , interpolation=cv2.INTER_AREA)
     x_test = resized_ti.reshape(1,-1)
-    # YOUR CODE HERE 
+    x_test = torch.from_numpy(x_test).double()
+    if loss_mode == 'crossentropy':
+        y_test = np.array([ti[1]])  
+        y_test = torch.from_numpy(y_test)
+    else:
+        y_test = np.array(ti[1])  
+        y_test = torch.from_numpy(y_test).double()
+    
+  
+    loss_test, a1_test, a2_test = forward(x_test,y_test,W1.double(), W2.double(), b1.double(), b2.double() )
     # convert test images to pytorch
     # do forward pass depending mse or softmax
-    # print("Test output (values / pred_id / true_id):", a2_test, np.argmax(a2_test), ti[1])
+    print("Test output (values / pred_id / true_id):", a2_test, np.argmax(a2_test), ti[1])
 
-# print("------------------------------------")
-# print("Test model output Weights:", W1, W2)
-# print("Test model output bias:", b1, b2)
+print("------------------------------------")
+print("Test model output Weights:", W1, W2)
+print("Test model output bias:", b1, b2)
 
 
 plt.title("Training Loss vs. Number of Training Epochs")
@@ -189,4 +243,3 @@ plt.xticks(np.arange(1, num_epochs+1, 50.0))
 plt.legend()
 plt.show()
 plt.savefig("simple_nn_train.png")
-
